45fa0cd429be765a2083bd1f52049c37dbd6970a
Philip Sephton <psephton@academytrainee.com>
2025-11-29 14:39:54 +0000

    Added from spring initializer

---
+++ ESGTest/src/main/java/com/esg/test/ESGTest/EsgTestApplication.java
@@ -0,0 +1,13 @@
+package com.esg.test.ESGTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgTestApplication.class, args);
+	}
+
+}
---
+++ ESGTest/src/test/java/com/esg/test/ESGTest/EsgTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esg.test.ESGTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsgTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
